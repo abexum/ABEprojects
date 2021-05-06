40c3f7a315741fcde47a19d45ee49dc5a26bdcfd
abexum <abexum@gmail.com>
2021-05-06 16:24:47 -0500

    make the app pretty

--- shoppingBasket/src/app/app.module.ts
+++ shoppingBasket/src/app/app.module.ts
@@ -5,18 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoodsItemComponent } from './goods-item/goods-item.component';
 import { StoreComponent } from './store/store.component';
-import { ReceiptComponent } from './receipt/receipt.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbCardModule, NbButtonModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   declarations: [
     AppComponent,
     GoodsItemComponent,
-    StoreComponent,
-    ReceiptComponent
+    StoreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbCardModule,
+    NbButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- shoppingBasket/src/app/goods-item/goods-item.component.ts
+++ shoppingBasket/src/app/goods-item/goods-item.component.ts
@@ -3,14 +3,18 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-goods-item',
   template: `
-    <button>{{ title }}<br>$ {{ price }}</button>
+    <button nbButton hero status="warning" *ngIf="!taxExempt && !imported">{{ title }}<br>$ {{ price }}</button>
+    <button nbButton hero status="success" *ngIf="taxExempt && !imported">{{ title }}<br>$ {{ price }}</button>
+    <button nbButton hero status="info" *ngIf="taxExempt && imported">{{ title }}<br>$ {{ price }}</button>
+    <button nbButton hero status="primary" *ngIf="!taxExempt && imported">{{ title }}<br>$ {{ price }}</button>
   `,
-  styles: [
-  ]
+  styles: ['button { width: 100%; height: 100%; font-size: 1em; }']
 })
 export class GoodsItemComponent {
 
   @Input() title: string = '';
   @Input() price: string = '';
+  @Input() taxExempt: boolean = false;
+  @Input() imported: boolean = false;
 
 }
